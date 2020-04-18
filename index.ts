ba2fa023d24af13d007cd7674bf591f61c2756e7
Paul Pan <panjunhong05@gmail.com>
2020-04-17 18:00:13 -0700

    Added commands to package.json and imported aws-sdk

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import awsServerlessExpress = require('aws-serverless-express');
+import AWS = require('aws-sdk');
 import app from './app';
+
 const server = awsServerlessExpress.createServer(app);
 
-const handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) };
 
-export { handler };
+
+export const handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
