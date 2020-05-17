176889bbfa7c08205c675f3f71d8b3d26bf3cbdb
Paul Pan <panjunhong05@gmail.com>
2020-05-16 21:41:12 -0700

    Added listings database definitions and removed listing details table

--- src/db/listings.ts
+++ src/db/listings.ts
@@ -6,6 +6,37 @@ class Listings {
   constructor(docClient: DynamoDB.DocumentClient) {
     this.docClient = docClient;
   }
+
+  async createListing(
+    listingId: string,
+    userId: string,
+    title: string,
+    price: number,
+    description: string,
+    location: string,
+    tags: [string],
+    pictures: [string],
+  ) {
+    const params = {
+      TableName: 'TEListingsTable',
+      Item: {
+        listingId,
+        userId,
+        title,
+        price,
+        description,
+        location,
+        tags,
+        pictures,
+        creationDate: Date.now(),
+        sold: false,
+        soldTo: null,
+        comments: [],
+        SavedCount: 0,
+      },
+    };
+    await this.docClient.put(params).promise();
+  }
 }
 
 export default Listings;
