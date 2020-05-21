1ff608099ac694ba28ed4f196c3876ed77c6a7fc
Paul Pan <panjunhong05@gmail.com>
2020-05-20 17:40:34 -0700

    Added additional tests

--- test/api/v1/index.ts
+++ test/api/v1/index.ts
@@ -1,6 +1,8 @@
 import root from './root';
+import user from './user';
 
 export default () =>
   describe('API Tests', () => {
     root();
+    user();
   });
--- test/db/user.ts
+++ test/db/user.ts
@@ -1,3 +1,4 @@
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import Users from '../../src/db/user';
 
 const dynamodb = (process as any).dynamodb as AWS.DynamoDB.DocumentClient;
@@ -5,16 +6,27 @@ const dynamodb = (process as any).dynamodb as AWS.DynamoDB.DocumentClient;
 export default () =>
   describe('Test the TEUsersTable', () => {
     it('User insertion test', async () => {
-      await new Users(dynamodb).createProfile(
-        'asdfsaf',
-        'Paul Pan',
-        'panjunhong05@gmail.com',
-        '+1 (626) 691-8088',
-        'Los Angeles, CA',
-        'hello.jpg',
-      );
-      const result = await new Users(dynamodb).getProfile('asdfsaf');
+      try {
+        await new Users(dynamodb).createProfile(
+          'asdfsaf',
+          'Paul Pan',
+          'panjunhong05@gmail.com',
+          '+1 (626) 691-8088',
+          'Los Angeles, CA',
+          'hello.jpg',
+        );
+      } catch (err) {
+        expect(err).toBe(undefined);
+      }
+    });
+
+    it('Get user test', async () => {
+      let result: DocumentClient.AttributeMap | undefined;
+      try {
+        result = await new Users(dynamodb).getProfile('asdfsaf');
+      } catch (err) {
+        expect(err).toBe(undefined);
+      }
       expect(result!.name).toBe('Paul Pan');
-      return true;
     });
   });
