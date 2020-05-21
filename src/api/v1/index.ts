7abd411d2f162b74cf20816158a96d15596f88e9
Paul Pan <panjunhong05@gmail.com>
2020-05-21 15:14:03 -0700

    Added several database functions

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
 import authenticate from './authenticate';
 import user from './user';
+import listings from './listings';
 
 const router = express.Router();
 
 router.use('/user', authenticate, user);
+
+router.use('/listings', listings);
 
 export default router;
