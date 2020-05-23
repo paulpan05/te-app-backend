7ed0f574aa01aaafa63562a6c2a502ebe99728c8
Paul Pan <panjunhong05@gmail.com>
2020-05-22 21:28:44 -0700

    Added remove listings

--- src/api/users.ts
+++ src/api/users.ts
@@ -119,4 +119,31 @@ router.post('/save-listing', async (req, res, next) => {
   }
 });
 
+router.post('/unsave-listing', async (req, res, next) => {
+  if (!req.body) {
+    return next(new HttpError.BadRequest('Missing body'));
+  }
+  const { listingId, creationTime } = req.body;
+  if (!listingId) {
+    return next(new HttpError.BadRequest('Missing listingId'));
+  }
+  if (!creationTime) {
+    return next(new HttpError.BadRequest('Missing creationTime'));
+  }
+  try {
+    await UsersTable.removeSavedListing(res.locals.userId, listingId);
+    await ListingsTable.decrementSavedCount(listingId, creationTime);
+    return res.send({ message: 'Success' });
+  } catch (err) {
+    const castedError = err as AWSError;
+    return next(
+      new HttpError.Custom(
+        castedError.statusCode || config.constants.INTERNAL_SERVER_ERROR,
+        castedError.message,
+        castedError.code,
+      ),
+    );
+  }
+});
+
 export default router;
