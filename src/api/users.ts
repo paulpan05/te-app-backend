218f480112cb4841634ace8c54419ff717a21a94
Paul Pan <panjunhong05@gmail.com>
2020-05-22 19:44:47 -0700

    Added save listings

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,6 +1,6 @@
 import * as express from 'express';
 import { AWSError } from 'aws-sdk';
-import { UsersTable } from '../db';
+import { UsersTable, ListingsTable } from '../db';
 import HttpError from '../error/http';
 import config from '../config';
 
@@ -79,6 +79,33 @@ router.post('/signup', async (req, res, next) => {
       location,
       customPicture || picture,
     );
+    return res.send({ message: 'Success' });
+  } catch (err) {
+    const castedError = err as AWSError;
+    return next(
+      new HttpError.Custom(
+        castedError.statusCode || config.constants.INTERNAL_SERVER_ERROR,
+        castedError.message,
+        castedError.code,
+      ),
+    );
+  }
+});
+
+router.post('/save-listing', async (req, res, next) => {
+  if (!req.body) {
+    return next(new HttpError.BadRequest('Missing body'));
+  }
+  const { listingId, creationTime } = req.body;
+  if (!listingId) {
+    return next(new HttpError.BadRequest('Missing listingId'));
+  }
+  if (!creationTime) {
+    return next(new HttpError.BadRequest('Missing creationTime'));
+  }
+  try {
+    await UsersTable.addSavedListing(res.locals.userId, listingId);
+    await ListingsTable.incrementSavedCount(listingId, creationTime);
     return res.send({ message: 'Success' });
   } catch (err) {
     const castedError = err as AWSError;
